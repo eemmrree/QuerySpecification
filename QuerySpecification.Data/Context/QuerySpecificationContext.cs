fa2b943c6aa8781d59537903db961db5424aef86
Emre Bayrak <eemmrree0@outlook.com>
2022-01-06 14:25:18 +0300

    context sınıfı oluşturuldu

---
+++ QuerySpecification.Data/Context/QuerySpecificationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuerySpecification.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuerySpecification.Data.Context
+{
+    public class QuerySpecificationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Integrated Security=True");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
