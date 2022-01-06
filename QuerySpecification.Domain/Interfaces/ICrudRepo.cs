a94fb220a184757c5ddca17573e53cf23be36ddd
Emre Bayrak <eemmrree0@outlook.com>
2022-01-06 14:26:34 +0300

    generic interface repolar eklendi

---
+++ QuerySpecification.Domain/Interfaces/ICrudRepo.cs
@@ -0,0 +1,20 @@
+﻿using QuerySpecification.Domain.QuerySpecification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuerySpecification.Domain.Interfaces
+{
+    public interface ICrudRepo<T>
+    {
+        T Find(Specification<T> specification);
+        T FindAsEntity(Specification<T> specification);
+        IEnumerable<T> FindAll(Specification<T> specification);
+        IEnumerable<T> FindAllAsEntity(Specification<T> specification);
+        IEnumerable<T> FindAll(Expression<Func<T,bool>> expression);
+        IEnumerable<T> FindAllAsEntity(Expression<Func<T,bool>> expression);
+    }
+}
---
+++ QuerySpecification.Domain/Interfaces/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuerySpecification.Domain.Interfaces
+{
+    public interface IRepo<T>
+    {
+        IEnumerable<T> All();
+        T Find(Expression<Func<T, bool>> expression);
+        T Get(int id);
+        void Rollback(T entity);
+    }
+}
