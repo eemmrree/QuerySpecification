f741f734ba620395887fbe02d9150695705ccc2f
Emre Bayrak <eemmrree0@outlook.com>
2022-01-06 14:25:55 +0300

    entity eklendi

---
+++ QuerySpecification.Domain/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuerySpecification.Domain.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+
+    }
+}
