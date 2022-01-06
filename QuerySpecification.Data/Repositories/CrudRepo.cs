63ea7a7da0901ad200fc20daff5b7a9b4cb662dd
Emre Bayrak <eemmrree0@outlook.com>
2022-01-06 14:26:55 +0300

    generic repository sınıfı yazıldı

---
+++ QuerySpecification.Data/Repositories/CrudRepo.cs
@@ -0,0 +1,45 @@
+﻿using QuerySpecification.Domain.Interfaces;
+using QuerySpecification.Domain.QuerySpecification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuerySpecification.Data.Repositories
+{
+    public abstract class CrudRepo<T> : ICrudRepo<T>
+    {
+
+        public T Find(Specification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindAll(Specification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindAll(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindAllAsEntity(Specification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> FindAllAsEntity(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindAsEntity(Specification<T> specification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
