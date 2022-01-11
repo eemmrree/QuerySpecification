557826108588815617e53ebf9567c67319f0da99
Emre Bayrak <eemmrree0@outlook.com>
2022-01-11 16:12:06 +0300

    productdto added

---
+++ QuerySpecification.Application/Dto/ProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuerySpecification.Application.Dto
+{
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+        public int UnitsOnOrder { get; set; }
+    }
+}
