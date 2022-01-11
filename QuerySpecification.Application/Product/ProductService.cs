0069ca3b4cd95327d88492da08a34afdbc435a4f
Emre Bayrak <eemmrree0@outlook.com>
2022-01-11 15:57:48 +0300

    ProductService eklendi.

---
+++ QuerySpecification.Application/Product/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using QuerySpecification.Data.Repositories;
+using QuerySpecification.Domain.Entities;
+using QuerySpecification.Domain.QuerySpecification;
+
+namespace QuerySpecification.Application
+{
+    public class ProductService : IProductService
+    {
+        private readonly CrudRepo<Product> _crudRepo;
+
+        public ProductService(CrudRepo<Product> crudRepo)
+        {
+            _crudRepo = crudRepo;
+        }
+
+        public void GetAll()
+        {
+            var specification = new AllProductSpec();
+            var specUnitsInStock = new InStockProductSpec();
+            _crudRepo.FindAll(specification.And(specUnitsInStock));
+        }
+    }
+}
