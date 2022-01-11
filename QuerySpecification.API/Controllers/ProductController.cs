23343aaf095aee6539dcc5e35091fa7e051c078e
Emre Bayrak <eemmrree0@outlook.com>
2022-01-11 16:11:53 +0300

    product controller added

---
+++ QuerySpecification.API/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuerySpecification.Application;
+using QuerySpecification.Application.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuerySpecification.API.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult FindAllProduct()
+        {
+            List<ProductDto> products = _productService.GetAll();
+            return View(products);
+        }
+       
+    }
+
+}
